f18fd68b2c7995d9b61fb9d8e0cd1ebf017f18b6
houfengtai <houfengtai@sumawuliu.com>
2018-02-09 10:25:33 +0800

    update setting

---
+++ server/settings.js
@@ -0,0 +1,11 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+module.exports = {
+  cookieSecret: 'myblog',
+  db: 'microblog',
+  host: '192.168.3.185',
+  port: 27017,
+  url: 'mongodb://192.168.3.185:27017/microblog'
+}
